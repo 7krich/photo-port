0c5e7c701b74cd33824f44ecf3bc68d6c83ada10
Kyle Richnafsky <7krich@gmail.com>
2022-06-14 10:33:06 -0400

    updated formState changes for all input fields

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ContactForm() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
+
+    // sync the internal state of the component formState with the user input from the DOM
+    // onChange event listener will ensure that the handleChange function fires when keystroke is typed into input field
+    function handleChange(e) {
+        // dynamically obtain keystrokes from each given form element
+        // e.target.name -> name refers to the name attribute for the form elements (name, email, message)
+        setFormState({...formState, [e.target.name]: e.target.value })
+    }
+    console.log(formState)
     return(
         <section>
             <h1>Contact me</h1>
             <form id="contact-form">
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" defaultValue={name} name="name" onChange={handleChange}/>
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" />
+                    <input type="email" defaultValue = {email} name="email" onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" />
+                    <textarea name="message" defaultValue = {message} onChange={handleChange} rows="5" />
                 </div>
                 <button type="submit">Submit</button>
             </form>
